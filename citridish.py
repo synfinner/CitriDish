ddf84f26df30d3898a965c3186b2c3fd3c0df713
synfinner <synfinner@synfinner.ru>
2023-07-21 08:57:49 -0400

    Add DTCERT last modified check

--- citridish.py
+++ citridish.py
@@ -3,7 +3,10 @@
 import argparse
 import requests
 import sys
-import ssl
+import datetime
+
+# Author: @synfinner
+# Credit to Deutsche Telekom CERT for the patched versions array
 
 # Suppress insecure request warnings
 requests.packages.urllib3.disable_warnings()
@@ -15,6 +18,12 @@ CGW_PATH = "/vpn/logout.html"
 # Citrix AAA path
 AAA_PATH = "/logon/LogonPoint/tmindex.html"
 
+# Patched versions array. Credit to Deutsche Telekom CERT
+PATCHED_VERSIONS = [
+    {"version": "13.0-91.13", "timestamp": "Fri, 07 Jul 2023 15:39:40 GMT"},
+    {"version": "13.1-49.13", "timestamp": "Mon, 10 Jul 2023 17:41:17 GMT"},
+    {"version": "13.1-49.13", "timestamp": "Mon, 10 Jul 2023 18:36:14 GMT"}
+]
 
 def check_citrix(target):
     # Define url variable with https and the ip address or hostname
@@ -33,7 +42,18 @@ def check_citrix(target):
             # If the status code is 200, check for specific content to identify it as a citrix gateway
             if "<title>Citrix Gateway</title>" in cgw_response.text or "/vpn/js/logout_view.js?v=" in cgw_response.text:
                 # Print the host or ip and that it is a citrix gateway
-                print(target + " - Citrix Gateway identified")
+                # set last_modified to the last modified header in datetime format
+                last_modified = datetime.datetime.strptime(cgw_response.headers["Last-Modified"], "%a, %d %b %Y %H:%M:%S %Z")
+                potentially_vuln = False
+                # Loop through the patched versions array and if the last_modified variable is less than the timestamp set potentially_vuln to True
+                for patched_version in PATCHED_VERSIONS:
+                    if last_modified < datetime.datetime.strptime(patched_version["timestamp"], "%a, %d %b %Y %H:%M:%S %Z"):
+                        potentially_vuln = True
+                # If potentially_vuln is True, print the host or ip and that it is a citrix gateway and potentially vulnerable
+                if potentially_vuln:
+                    print(target + " - Potentially vulnerable Citrix Gateway identified (CVE-2023-3519)")
+                else:
+                    print(target + " - Citrix Gateway identified")
 
         if aaa_response.status_code == 200:
             # If the status code is 200, check for specific content to identify it as a citrix aaa
